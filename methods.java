454eb6c53929ab7a3ef6f383516f2e72ab2f05a5
Sarder32 <ibrahim.mukut@gmail.com>
2016-12-16 09:46:49 -0500

    Add files via upload

---
+++ methods.java
@@ -0,0 +1,193 @@
+
+<HEAD>
+   <meta name="DCSext.pgf_Site" content="Global Home">
+ <meta name="DCSext.pCC" content="US">
+ <meta name="DCSext.pLL" content="en">
+ <meta name="DCSext.pSA" content="Global Entry Point">
+ <meta name="DCSext.pSU" content="Global Entry">
+ <meta name="DCSext.pPID" content="homepage/ent.html">
+ <meta name="DCSext.pPS" content="0">
+ <meta name="DCSext.pCS" content="Unknown">
+ <meta name="DCSext.pSS" content="4400,C3">
+ <meta name="DCSext.pJSV" content="1016">
+ <meta name="DCSext.pCV" content="1016">
+ <meta name="DCSext.pLI" content="1">
+ 
+
+   
+   
+   
+   <script src="/javascript/global_entry.js?V=1016" type="text/javascript"></script>
+
+   <meta name="description" content="logistics, tracking, shipping">
+
+   <META HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=utf-8">
+   <TITLE>Shipping, Freight, Logistics and Supply Chain Management from S2H24</TITLE>
+   <script  src="//tags.tiqcdn.com/utag/s2h24/main/prod/utag.sync.js" type="text/javascript" charset="utf-8"></script>
+
+
+</HEAD>
+<body>
+
+<a href="#skip_link_content" id="skip_link" tabindex="0">Skip to Content</a>
+
+<table border="0" cellpadding="0" cellspacing="0" width="100%" align="center">
+	<tr>
+		<div id="skip_link_content">
+		
+		
+		<!-- SearchBodyStart -->
+		<td>
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
+
+
+
+
+<noscript>
+<link rel="stylesheet" type="text/css" href="/stylesheets/globalhomepage.css?V=16.07.15.0"/>
+</noscript>
+
+<form name="frmCountry" onsubmit="return popLanguage(frmCountry, frmCountry.cookie, frmCountry.setCookie);" action="/">
+<input type="hidden" name="Site" value="Corporate">
+
+<table border="0" cellpadding="0" cellspacing="0" width="100%" class="brand">
+	<tr>
+		<td><div align="center"><img src="/img/s2h24_logo.gif" alt="UPS Shield Logo"></div></td>
+	</tr>
+	<tr>
+		<td><div align="center"><img src="/img/s2h24_tag_line.gif" alt=""></div></td>
+	</tr>
+</table>
+
+<div class="countrySelection">
+<table border="0" cellpadding="0" cellspacing="0">
+	<tr>
+		<td class="countrySelectCell">
+		<div class="modulepad countrySelect">
+        &nbsp;<select name="cookie" size="1" style="width:300px;font-size:9pt;">
+<option>Select your location</option>
+<option value="us_en_home">United States - English</option>
+<option value="us_es_home_none">United States - Spanish</option>
+
+<option value="wf_en_home_none">Wallis and Futuna - English</option>
+<option value="xy_en_home_none">Yap - English</option>
+
+		</select>
+		</div>
+		</td>
+		<td class="countrySelectCell"><input alt="Go" name="inputImgTag" value="" class="arrowBtnMini" type="submit"></td>
+	</tr>
+	<tr><td colspan="2" class="countryCheckCell"><img alt="" border="0" height="8" src="/img/1.gif" width="10"><br>
+	<div class="modulepad countryCheck">
+	<input class="rad" name="setCookie" type="checkbox" checked="checked" value="yes">&nbsp;Remember this location.<br>
+	</div>
+	</td></tr></table>
+</div>
+
+</form>
+					
+		</td>
+		<td><img alt="" border="0" height="1" src="/img/1.gif" width="10"></td>
+	</tr>
+</table>
+
+
+
+
+
+</td>
+		<!-- SearchBodyEnd -->
+		
+		
+		</div>
+	</tr>
+	<tr>
+		<td>
+			<div align="center">
+			<table border="0" cellpadding="0" cellspacing="0">
+				<tr>
+					<td><img alt="" border="0" height="1" src="/img/1.gif" width="660"></td>
+				</tr>
+				<tr>
+					<td align="left">
+
+
+
+
+
+
+
+
+<table border="0" class="ccfmbg1" cellpadding="0" cellspacing="0"
+		width="100%">
+		<tr>
+			<td colspan="3"><img alt="" border="0" height="19"
+				src="/img/1.gif" width="1"></td>
+		</tr>
+		<tr>
+			<td height="1" rowspan="4"><img alt="" border="0" height="1"
+				src="/img/1.gif" width="10"></td>
+			<td class="ccfmln1"><img alt="" border="0" height="1"
+				src="/img/1.gif" width="1"></td>
+			<td height="1" rowspan="4"><img alt="" border="0" height="1"
+				src="/img/1.gif" width="10"></td>
+		</tr>
+		<tr>
+			<td><img alt="" border="0" height="10" src="/img/1.gif"
+				width="1"></td>
+		</tr>
+		<tr>
+			<td><span class="ccfmtxt1">
+			  
+ 			 
+			 
+			 
+			<br>
+			</span> <br>
+			<span class="ccmftxt2">
+				Copyright &copy; 1994-2016 United Parcel Service of America, Inc. All rights reserved.
+			 </span><br>
+			 <a href="javascript:popWrapper('https://www.ups.com/content/us/en/use.html?WT.svl=Footer','pop_up_3_col')" style="white-space: nowrap;">Web Site Terms of Use</a>							&nbsp;| <a href="javascript:popWrapper('https://www.ups.com/content/corp/privacy_policy.html?WT.svl=Footer','pop_up_3_col')" style="white-space: nowrap;">Privacy Notice</a>							&nbsp;| <a href="javascript:popWrapper('https://www.ups.com/content/us/en/index.jsx?WT.svl=Footer','pop_up_3_col')" style="white-space: nowrap;">U.S. Home</a>							&nbsp;| <a href="javascript:popWrapper('https://www.ups.com/content/us/en/bussol/browse/solutions/index.html?WT.svl=Footer','pop_up_3_col')" style="white-space: nowrap;">UPS Solutions</a>			</td>
+		</tr>
+		<tr>
+			<td><img alt="" border="0" height="10" src="/img/1.gif"
+				width="1"></td>
+		</tr>
+	</table>
+<script src="/assets/tealium/tealium_wt.js" type="text/javascript" charset="utf-8"></script>
+
+
+
+
+
+
+
+<script> var preferred=false; var national=false; var worldship=false; </script>
+ 
+
+
+</td>
+				</tr>
+			</table>
+			</div>
+		</td>
+	</tr>
+</table>
+<img alt="" border="0" src="/img/icp.gif">
+</BODY>
+
+</HTML>
